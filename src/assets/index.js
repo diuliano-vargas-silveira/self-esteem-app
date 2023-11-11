8b50016d4a2e57c087ed717f8964c3af94d31510
nathanmintegui <nathanberger.rs@hotmail.com>
2023-11-11 18:33:54 -0300

    add menu behavior on mouse hover

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 import iconeInstagram from "./icones/instagram.icone.svg";
 import imagemLogo from "./icones/logo.imagem.png";
+import iconeInteracaoMenu from "./icones/interacao.menu.icone.svg";
 
-export { iconeInstagram, imagemLogo };
+export { iconeInstagram, imagemLogo, iconeInteracaoMenu };
